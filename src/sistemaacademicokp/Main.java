a34774244b53352975ec62668c0b127679b9651e
Kenys Padilla <kvpadilla@unicesar.edu.co>
2026-09-24 22:25:40 -0500

    feat: crear estructura base de Main con ArrayLists

--- src/sistemaacademicokp/Main.java
+++ src/sistemaacademicokp/Main.java
@@ -1,18 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package sistemaacademicokp;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- *
- * @author vanes
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    private static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    private static ArrayList<Nota> notas = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
